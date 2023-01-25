c39992f75a96054fb6a0f9608f2c38bcbc0db10b
izkeas <izkeasyomito@gmail.com>
2023-01-24 21:19:21 -0300

    edited backend config

---
+++ backend/src/config.d.ts
@@ -0,0 +1,8 @@
+export declare const dev : boolean;
+export declare const name : string;
+export declare const http : string;
+export declare const hostname : string;
+export declare const backendPort : number;
+export declare const backendURL : string;
+export declare const frontendPort : number;
+export declare const frontendURL : string;
