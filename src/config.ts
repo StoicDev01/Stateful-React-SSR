27c97e9417f9acda2e149d154f3f1bc9d81ba008
izkeas <izkeasyomito@gmail.com>
2023-01-26 23:02:25 -0300

    Changed config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,15 @@
 export const dev = process.env.NODE_ENV !== 'production';
 
 /* CONFIGS */
-const productionHighPorts = true;       // Choose to use high ports like 3000 instead of 443 or 80
+const productionHighPorts = false;       // Choose to use high ports like 3000 instead of 443 or 80
 const productionUseIp = false;
 const productionUseHttp = false;
 const localIpAddr = "192.168.0.10";
-export const name = "ChaoticForge"
 
+export const name = "ChaoticForge"
 export const http = (dev || productionUseHttp) ? "http" : "https";
 export const hostname = (dev || productionUseIp) ? localIpAddr : `${name.toLowerCase()}.onrender.com`;
 
-export const hostPort = (dev || productionHighPorts) ? 3000 : 443;
-export const productionPort = 443;
+export const hostPort = 3000;
+export const productionPort = productionHighPorts ? 3000 : 443;
 export const url = `${http}://${hostname}:${ dev ? hostPort : productionPort}`;
